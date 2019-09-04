3324e27bb6177dff421a87a92e7abfd1530de71b
Thomas <thomy.ripari@outlook.com>
2019-09-04 00:57:09 -0300

    Complete NAs with Mean or Median, Create a pattern of dataframe NAs and plotting this pattern

--- scripts/first-theme/12-missing-data-2.R
+++ scripts/first-theme/12-missing-data-2.R
@@ -23,3 +23,37 @@ summary(data)
 rows_clean = c("medv", "lstat")
 data_clean_rows = data[, !(names(data) %in% rows_clean)]
 names(data_clean_rows)
+
+
+# Complete NAs with Mean or Median
+install.packages("Hmisc")
+library(Hmisc)
+
+housing = read.csv("../data/first-theme/housing-with-missing-value.csv", header = TRUE, stringsAsFactors = FALSE)
+
+housing_copy_1 = housing
+summary(housing_copy_1)
+
+housing_copy_1$ptratio = impute(housing_copy_1$ptratio, mean)
+housing_copy_1$rad = impute(housing_copy_1$rad, median)
+summary(housing_copy_1)
+
+
+# Get a Pattern of Dataframe
+install.packages("mice")
+library(mice)
+
+md.pattern(housing)
+
+# Plots with NAs
+
+install.packages("VIM")
+library(VIM)
+
+aggr(housing, 
+     col = c('White', 'Black'),
+     numbers = TRUE,
+     sortVars = TRUE,
+     cex.axis = 0.75,
+     gap = 1,
+     ylab = c("Histograma de NAs", "Patron de NAs"))
