e9d2be77dee7d15948c35a5a1c4ea70bc18fabc0
Thomas <thomy.ripari@outlook.com>
2019-09-07 00:47:08 -0300

    replacement of NAs through the predictive comparison of means (PMM) using Mice

---
+++ scripts/first-theme/14-imputing-data.R
@@ -0,0 +1,25 @@
+library(mice)
+
+housing.data = read.csv("../data/first-theme/housing-with-missing-value.csv", header = TRUE, stringsAsFactors = FALSE)
+
+summary(housing.data)
+
+columns = c("rad", "ptratio")
+
+imputed_data = mice(housing.data[, names(housing.data) %in% columns],
+                    m = 5,
+                    maxit = 50,
+                    method = "pmm",
+                    seed = 2018
+                    )
+
+# pmm - Comparacion predictiva de medias
+
+
+complete.data = mice::complete(imputed_data)
+
+
+housing.data$rad = complete.data$rad
+housing.data$ptratio = complete.data$ptratio
+
+anyNA(housing.data)
