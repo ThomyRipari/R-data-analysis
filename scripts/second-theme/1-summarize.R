532c7354b9fcd8a34f30a7a86bace47e9e9fde4a
Thomas <thomy.ripari@outlook.com>
2019-09-10 00:35:43 -0300

    Descriptive Statistics (Measures of Centralization, Dispersion and Asymmetry)

--- scripts/second-theme/1-summarize.R
+++ scripts/second-theme/1-summarize.R
@@ -11,3 +11,52 @@ summary(data$cylinders)
 # Structure of dataframe
 str(data)
 str(data$cylinders)
+
+
+install.packages(c("raster", "moments"))
+library(raster)
+library(moments)
+
+install.packages("devtools")
+library(devtools)
+devtools::install_github("paulponcet/modeest")
+library(modeest)
+
+X = data$mpg
+
+# Medidas de Centralizacion
+
+# Promedio
+mean(X)
+
+# Mediana
+median(X)
+
+# Moda, utilizando paquete modeest
+mfv(X)
+
+# Quantiles, utilizando paquete raster
+quantile(X)
+
+# Medidas de Dispersion
+
+# Varianza
+var(X)
+
+# Desviacion Tipica
+sd(X)
+
+# Coeficiente de varianza
+cv(X)
+
+# Medidas de Asimetria, usando moments
+
+# Asimetria de Fisher
+skewness(X)
+
+# Curtosis
+moments::kurtosis(X)
+
+# Histograma
+hist(X)
+
